3413968158aa7968e542b9fa52188918700f7eed
openbit-hu <72387216+openbit-hu@users.noreply.github.com>
2021-02-06 07:58:44 +0100

    main.ts, test.ts frissítése

--- main.ts
+++ main.ts
@@ -17,11 +17,20 @@ class OBScreen extends OBImage {
     }
 }
 namespace obDisplay{
-    let id = 0;
+    let id: number;
+    let screen: OBScreen;
+    let x: number;
+    let y: number;
+    radio.onReceivedString(function (receivedString: string) {
+        led.plotBrightness(2, 2, 255)
+    })
     export function initMaster(){
-
+        radio.setGroup(4);
+        radio.sendString("Hello");
     }
     export function initSlawe(){
+        radio.setGroup(4);
+        id = 0
         basic.showNumber(id);
         while(true){
             control.waitMicros(1000);
@@ -30,7 +39,19 @@ namespace obDisplay{
                 basic.showNumber(id);
             }
             if(input.buttonIsPressed(Button.B)){
+                let rows = screen.width / 5;
                 return;
+            }
+        }
+    }
+    export function drawDisplay(){
+        //Adat a screen változóban
+        for(let i = screen.x; i < screen.x + screen.width; i++){
+            for(let j = screen.y; j < screen.y + screen.height; j++){
+                let index = j * screen.width + i;
+                let ch = screen.data.charAt(index);
+                let brightness = parseInt(ch) * 16;
+                led.plotBrightness(i - x, j - y, brightness);
             }
         }
     }
